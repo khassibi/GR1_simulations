1abd275c88e2dbff003ce66c974789efeeaeaeb1
kimiahassibi <khassibi@caltech.edu>
2023-07-27 12:39:08 -0700

    First commit

---
+++ graph_builder.py
@@ -0,0 +1,443 @@
+try:
+    from collections.abc import Sequence
+except ImportError:
+    from collections import Sequence
+import copy
+from itertools import chain
+import logging
+
+import networkx as nx
+
+from omega.logic import syntax as stx
+from omega.symbolic import prime as prm
+from omega.symbolic import symbolic
+
+from omega.games import enumeration as enum
+from omega.symbolic import enumeration as sym_enum
+
+log = logging.getLogger(__name__)  # TODO: see if this logger even works if I am calling enum functions too
+
+def _init_search(g, aut, umap, keys, qinit):
+    """Enumerate initial states according to `qinit`."""
+    # danger of blowup due to sparsity
+    # implement enumerated equivalent to compare
+    if qinit == '\A \E':
+        queue, visited = _forall_exist_init(g, aut, umap, keys)
+    elif qinit == '\A \A':
+        queue, visited = _forall_init(g, aut, umap, keys)
+    elif qinit == '\E \E':
+        queue, visited = _exist_init(g, aut, umap, keys)
+    elif qinit == '\E \A':
+        queue, visited = _exist_forall_init(g, aut, umap, keys)
+    else:
+        raise Exception('unknown qinit "{q}"'.format(q=qinit))
+    log.info('{n} initial nodes'.format(n=len(queue)))
+    return queue, visited
+
+def _forall_exist_init(g, aut, umap, keys):
+    r"""Enumerate initial states with \A env:  \E sys vars.
+
+    Note that each initial "state" is a class of
+    initial states in ZF set theory.
+    """
+    env_init = aut.init['env']
+    sys_init = aut.init['sys']
+    assert env_init != aut.false
+    assert sys_init != aut.false
+    # `env_init` should not depend on sys vars
+    only_env_init = aut.exist(aut.varlist['sys'], env_init)
+    env_iter = aut.pick_iter(
+        only_env_init, care_vars=aut.varlist['env'])
+    visited = aut.false
+    queue = list()
+    for env_0 in env_iter:
+        u = aut.let(env_0, sys_init)
+        sys_0 = aut.pick(u, care_vars=aut.varlist['sys'])
+        d = dict(env_0)
+        d.update(sys_0)
+        # confirm `sys_0` picked properly
+        u = aut.let(d, env_init)
+        assert u == aut.true, u
+        enum._add_new_node({**d, **{'shape': 'oval'}}, g, queue, umap, keys)
+        visited = enum._add_to_visited(d, visited, aut)
+    return queue, visited
+
+def _forall_init(g, aut, umap, keys):
+    r"""Enumerate initial states with \A \A vars."""
+    env_init = aut.init['env']
+    sys_init = aut.init['sys']  # to constrain only the
+        # initial value of the internal memory variables
+    assert env_init != aut.false
+    care_vars = chain(aut.varlist['env'], aut.varlist['sys'])
+    init_iter = aut.pick_iter(
+        env_init & sys_init,
+        care_vars=list(care_vars))
+    visited = aut.false
+    queue = list()
+    for d in init_iter:
+        enum._add_new_node({**d, **{'shape': 'oval'}}, g, queue, umap, keys)
+        visited = enum._add_to_visited(d, visited, aut)
+    return queue, visited
+
+def _exist_init(g, aut, umap, keys):
+    r"""Enumerate initial states with \E env, sys vars."""
+    sys_init = aut.init['sys']
+    assert sys_init != aut.false
+    care_vars = chain(aut.varlist['env'], aut.varlist['sys'])
+    d = aut.pick(
+        sys_init,
+        care_vars=list(care_vars))
+    visited = aut.false
+    queue = list()
+    enum._add_new_node({**d, **{'shape': 'oval'}}, g, queue, umap, keys)
+    visited = enum._add_to_visited(d, visited, aut)
+    return queue, visited
+
+def _exist_forall_init(g, aut, umap, keys):
+    r"""Enumerate initial states with \E sys:  \A env vars."""
+    # this function can be merged with `_forall_exist_init`
+    # by constraining initial sys assignments,
+    # then enumerating the same way
+    env_init = aut.init['env']
+    sys_init = aut.init['sys']
+    assert env_init != aut.false
+    assert sys_init != aut.false
+    # pick `sys_0` so that it work for all
+    # env assignments allowed by `env_init`
+    u = aut.forall(aut.varlist['env'], sys_init)
+    assert u != aut.false
+    sys_0 = aut.pick(u, care_vars=aut.varlist['sys'])
+    # iterate over env initial assignments
+    # allow `EnvInit` that depends on sys vars (Mealy env)
+    env_iter = aut.pick_iter(
+        env_init, care_vars=aut.varlist['env'])
+    visited = aut.false
+    queue = list()
+    for env_0 in env_iter:
+        d = dict(env_0)
+        d.update(sys_0)
+        # confirm `sys_0` works for all `env_0`
+        u = aut.let(d, env_init)
+        assert u == aut.true, u
+        enum._add_new_node({**d, **{'shape': 'oval'}}, g, queue, umap, keys)
+        visited = enum._add_to_visited(d, visited, aut)
+    return queue, visited
+
+def game_graph(aut, env, sys, qinit='\A \A'):
+    _aut = copy.copy(aut)
+    _aut.moore = aut.moore
+    _aut.varlist.update(
+        env=aut.varlist[env],
+        sys=aut.varlist[sys])
+    _aut.init.update(
+        env=aut.init[env],
+        sys=aut.init[sys])
+    _aut.action.update(
+        env=aut.action[env],
+        sys=aut.action[sys])
+    _aut.prime_varlists()
+    return _game_graph(_aut, qinit)
+
+def _game_graph(aut, qinit):
+    assert aut.action['sys'] != aut.false
+    primed_vars = enum._primed_vars_per_quantifier(aut.varlist)
+    vrs = set(aut.varlist['env']).union(aut.varlist['sys'])
+    unprime_vars = {stx.prime(var): var for var in vrs}
+    # fix an order for tupling
+    keys = list(vrs)
+    keys.append('shape')
+    umap = dict()  # map assignments -> node numbers
+    g = nx.MultiDiGraph()
+    queue, visited = _init_search(g, aut, umap, keys, qinit)
+    g.initial_nodes = set(queue)
+    varnames = set(vrs)
+    symbolic._assert_support_moore(aut.action['sys'], aut)
+    # search
+    while queue:
+        node = queue.pop()
+        values = {key: val for key, val in g.nodes[node].items() 
+                  if key not in ['color', 'shape']}
+        log.debug('at node: {d}'.format(d=values))
+        assert set(values) == varnames, (values, varnames)
+        u = aut.action['env']
+        u = aut.let(values, u)
+        # apply Mealy controller function
+        env_iter = aut.pick_iter(
+            u, care_vars=primed_vars['env'])
+        u = aut.action['sys']
+        assert u != aut.false
+        sys = aut.let(values, u)
+        if sys == aut.false:
+            g.nodes[node]['color'] = 'red'
+            continue
+        for next_env in env_iter:
+            log.debug('next_env: {r}'.format(r=next_env))
+            # no effect if `aut.moore`
+            u = aut.let(next_env, sys)
+            u = aut.let(unprime_vars, u)
+            env_values = {unprime_vars[var]: value
+                          for var, value in next_env.items()}
+            v = aut.let(env_values, visited)
+            v, _ = enum._select_candidate_nodes(
+                u, v, aut, visited=True)  # u is the next nodes and v is the visited nodes
+            sys_iter = aut.pick_iter(
+                v, care_vars=aut.varlist['sys'])
+
+            # Adding node with next environment action
+            # we want to take the system values from the node that pointed to this environment
+            # BUT only if that node was from a system move
+            d_env = dict(env_values)
+            d_sys = dict((k, values[k]) for k in set(aut.varlist['sys']))
+            d = {**d_env, **d_sys}
+            # assert
+            u = aut.let(d, visited)
+            assert u == aut.true or u == aut.false
+            # find or add node
+            env_node, already_visited = _get_node({**d, **{'shape': 'box'}}, g, umap, keys)  # TODO: maybe I don't need to check to find the node
+            if not already_visited:
+                visited = enum._add_to_visited(d, visited, aut)
+            c = remove_redundant_propositions(d_env)
+            g.add_edge(node, env_node, key="env", color='green', label=c)
+            
+            log.debug((
+                'next env: {e}\n'
+                'current sys: {s}\n').format(
+                e=env_values,
+                s=d_sys))
+
+            for next_sys in sys_iter:
+                # Adding node with next system action
+                d = {**d_env, **next_sys}
+                # assert
+                u = aut.let(d, visited)
+                assert u == aut.true or u == aut.false
+                # find or add node
+                sys_node, already_visited = _get_node({**d, **{'shape': 'oval'}}, g, umap, keys)  # TODO: maybe I don't need to check to find the node
+                if not already_visited:
+                    visited = enum._add_to_visited(d, visited, aut)
+                # Add a system node to the queue as long as the transition to it is unique
+                if not g.has_edge(env_node, sys_node, key='sys'):
+                    c = remove_redundant_propositions(next_sys)
+                    g.add_edge(env_node, sys_node, key="sys", color='blue', label=c)
+                    queue.append(sys_node)
+                # TODO: Make squares system and circles the environment and have separate nodes from which each player does an action
+
+                log.debug((
+                    'next env: {e}\n'
+                    'next sys: {s}\n').format(
+                    e=env_values,
+                    s=next_sys))
+
+    return g
+
+def state_graph(aut, env, sys, qinit='\A \A'):
+    r"""Return enumerated graph with steps as edges.
+
+    Only `aut.init['env']` considered.
+    The predicate `aut.init['sys']` is ignored.
+
+    `qinit` has different meaning that in `omega.games.gr1`.
+    Nonetheless, for synthesized `aut.init['env']`,
+    the meaning of `qinit` here yields the expected result.
+
+    Enumeration is done based on `qinit`:
+
+    - `'\A \A'`: pick all states that satisfy `aut.init[env] /\ aut.init[sys]`,
+      where the conjunct `aut.init[sys]` is included for the internal variables
+
+    - `'\E \E'`: pick one state that satisfies `aut.init[sys]`
+
+    - `'\A \E'`: for each environment state `x` that satisfies
+      `\E y:  aut.init[env]`,
+      pick a system state `y` that satisfies `aut.init[sys]`
+
+    - `'\E \A'`: pick a system state `y` that satisfies
+      `\A x:  aut.init[sys]`,
+      and enumerate all environment states `x` that satisfy `aut.init['env']`.
+    """
+    _aut = copy.copy(aut)
+    _aut.moore = aut.moore
+    _aut.varlist.update(
+        env=aut.varlist[env],
+        sys=aut.varlist[sys])
+    _aut.init.update(
+        env=aut.init[env],
+        sys=aut.init[sys])
+    _aut.action.update(
+        env=aut.action[env],
+        sys=aut.action[sys])
+    _aut.prime_varlists()
+    return _state_graph(_aut, qinit)
+
+def _state_graph(aut, qinit):
+    assert aut.action['sys'] != aut.false
+    primed_vars = enum._primed_vars_per_quantifier(aut.varlist)
+    vrs = set(aut.varlist['env']).union(aut.varlist['sys'])
+    unprime_vars = {stx.prime(var): var for var in vrs}
+    # fix an order for tupling
+    keys = list(vrs)
+    umap = dict()  # map assignments -> node numbers
+    g = nx.MultiDiGraph()
+    queue, visited = enum._init_search(g, aut, umap, keys, qinit)
+    g.initial_nodes = set(queue)
+    varnames = set(keys)
+    symbolic._assert_support_moore(aut.action['sys'], aut)
+    # search
+    while queue:  # TODO: check if omega combines all the states into one or if it skips some
+        node = queue.pop()
+        values = {key: val for key, val in g.nodes[node].items() 
+                  if key not in ['color']}
+        log.debug('at node: {d}'.format(d=values))
+        assert set(values) == varnames, (values, varnames)
+        u = aut.action['env']
+        u = aut.let(values, u)
+        # apply Mealy controller function
+        env_iter = aut.pick_iter(
+            u, care_vars=primed_vars['env'])
+        u = aut.action['sys']
+        assert u != aut.false
+        sys = aut.let(values, u)
+        if sys == aut.false:
+            g.nodes[node]['color'] = 'red'
+            continue
+        for next_env in env_iter:
+            log.debug('next_env: {r}'.format(r=next_env))
+            # no effect if `aut.moore`
+            u = aut.let(next_env, sys)
+            u = aut.let(unprime_vars, u)
+            env_values = {unprime_vars[var]: value
+                          for var, value in next_env.items()}
+            v = aut.let(env_values, visited)
+            v, _ = enum._select_candidate_nodes(
+                u, v, aut, visited=True)  # u is the next nodes and v is the visited nodes
+            sys_iter = aut.pick_iter(
+                v, care_vars=aut.varlist['sys'])
+
+            # Adding node with next environment action
+            # we want to take the system values from the node that pointed to this environment
+            # BUT only if that node was from a system move
+            d_env = dict(env_values)
+            d_sys = dict((k, values[k]) for k in set(aut.varlist['sys']))
+            d = {**d_env, **d_sys}
+            # assert
+            u = aut.let(d, visited)
+            assert u == aut.true or u == aut.false
+            # find or add node
+            env_node, already_visited = _get_node(d, g, umap, keys)
+            if not already_visited:
+                visited = enum._add_to_visited(d, visited, aut)
+            c = remove_redundant_propositions(d_env)
+            g.add_edge(node, env_node, key="env", color='green', label=c)
+            
+            log.debug((
+                'next env: {e}\n'
+                'current sys: {s}\n').format(
+                e=env_values,
+                s=d_sys))
+
+            for next_sys in sys_iter:
+                # Adding node with next system action
+                d = {**d_env, **next_sys}
+                # assert
+                u = aut.let(d, visited)
+                assert u == aut.true or u == aut.false
+                # find or add node
+                sys_node, already_visited = _get_node(d, g, umap, keys)
+                if not already_visited:
+                    visited = enum._add_to_visited(d, visited, aut)
+                # Add a system node to the queue as long as the transition to it is unique
+                if not g.has_edge(env_node, sys_node, key='sys'):
+                    c = remove_redundant_propositions(next_sys)
+                    g.add_edge(env_node, sys_node, key="sys", color='blue', label=c)
+                    queue.append(sys_node)
+
+                log.debug((
+                    'next env: {e}\n'
+                    'next sys: {s}\n').format(
+                    e=env_values,
+                    s=next_sys))
+
+    return g
+
+def _get_node(d, g, umap, keys):
+    """Add node to or find node in graph `g` for the assignment `d`."""
+    assert isinstance(keys, Sequence), keys
+    key = enum._node_tuple(d, keys)
+    if key in umap:
+        return umap[key], True
+    u = len(g)
+    assert u not in g, u
+    g.add_node(u, **d)
+    umap[key] = u
+    log.debug(d)
+    return u, False
+
+def remove_redundant_propositions(dict):
+    """Remove redundant propositions from `dict`
+    and return a list of strings of the non-redundant variable assignments.
+
+    @type dict: `dict`
+    """
+    c = []
+    for k, v in dict.items():
+        if type(v) == bool:
+            if v:
+                c.append(str(k))
+        elif k not in ['color', 'loc', 'eloc', 'shape']:
+            c.append('{var}={val}'.format(var=k, val=v))
+    return c
+
+def _game_format_nx(g, attributes=[]):
+    """Return game graph ready to be dumped.
+
+    Nodes with same label over `keys` are identified.
+    Edge attributes are copied.
+
+    @type g: `networkx.MultiDiGraph`
+    @param keys: `list` of keys in node attributes `dict` to
+        show as label, in the given order.
+        Attributes outside `keys` remain attributes.
+        By default all keys are shown.
+    @rtype: `pydot.Graph`
+    """
+    h = nx.MultiDiGraph()
+    for u, d in g.nodes(data=True):
+        c = remove_redundant_propositions(d)
+        s = sym_enum._square_conj(c)
+        attr = {k: v for k, v in d.items() if k in attributes}
+        attr.update(label=s)
+        h.add_node(u, **attr)
+    for u, v, attr in g.edges(data=True):
+        h.add_edge(u, v, **attr)
+    assert len(g) == len(h), (g.nodes, h.nodes, len(g), len(h))
+    return h
+
+def _state_format_nx(g, attributes=[]):
+    """Return graph ready to be dumped, where each state is a unique node.
+
+    Nodes with same label over `keys` are identified.
+    Edge attributes are copied.
+
+    @type g: `networkx.MultiDiGraph`
+    @param keys: `list` of keys in node attributes `dict` to
+        show as label, in the given order.
+        Attributes outside `keys` remain attributes.
+        By default all keys are shown.
+    @rtype: `pydot.Graph`
+    """
+    h = nx.MultiDiGraph()
+    umap = dict()
+    for u, d in g.nodes(data=True):
+        c = remove_redundant_propositions(d)
+        s = sym_enum._square_conj(c)
+        attr = {k: v for k, v in d.items() if k in attributes}
+        h.add_node(s, **attr)
+        umap[u] = s
+    for u, v, attr in g.edges(data=True):
+        us = umap[u]
+        vs = umap[v]
+        h.add_edge(us, vs, **attr)
+    assert len(g) >= len(h), (g.nodes, h.nodes)
+    assert len(g) == len(umap), (g.nodes, umap)
+    return h, umap
+
---
+++ runner_blocker.py
@@ -0,0 +1,74 @@
+import tulip as tlp
+from tulip.interfaces import omega as omega_int
+
+from tulip.interfaces import omega as omega_int
+import graph_builder as gb
+import networkx as nx
+
+def experiment():
+    # You can find the explainations of the states at GR1_simulations/RunnerBlockerStatesExplained.jpeg
+
+    # System definition
+    # Making a finite transition system
+    sys = tlp.transys.FTS()
+
+    sys.atomic_propositions.add_from({'a1', 'a2', 'a3', 'a4'})
+    sys.states.add('c1', ap={'a1'})
+    sys.states.add('c2', ap={'a2'})
+    sys.states.add('c3', ap={'a3'})
+    sys.states.add('c4', ap={'a4'})
+    sys.states.initial.add('c1')  # start in state c1
+
+    sys.transitions.add_comb({'c1'}, {'c2', 'c3'})
+    sys.transitions.add_comb({'c2'}, {'c4'})
+    sys.transitions.add_comb({'c3'}, {'c4'})
+    sys.transitions.add_comb({'c4'}, {'c4'})
+
+    # Specifications for the environment
+
+    # Blocker dynamics
+    env_vars = {'b': (1, 3)}
+    env_init = {'b = 2'}
+    env_safe = {
+        'b = 2 -> next(b) = 1 | next(b) = 3',
+        'b = 1 -> next(b) = 2',
+        'b = 3 -> next(b) = 2'
+    }
+    env_prog = {}
+    # QUESTION: Is this how I make env_prog?
+
+    # System variables and requirements
+    sys_vars = {}
+    sys_init = {}
+    sys_prog = {'a4'}
+    sys_safe = {
+        '!(a2 & b = 1)',
+        '!(a3 & b = 3)'
+    }
+
+    # Function found in tulip-control/tulip/spec/form.py
+    specs = tlp.spec.GRSpec(env_vars, sys_vars, env_init, sys_init,
+                            env_safe, sys_safe, env_prog, sys_prog)
+    specs.qinit = '\E \A'
+    specs.moore = True
+    print(specs.pretty())
+
+    spec = tlp.synth._spec_plus_sys(specs, None, sys, False, False)
+    # Automaton class found in omega/omega/symbolic/temporal.py
+    aut = omega_int._grspec_to_automaton(spec)
+    # Making a graph of the asynchronous GR(1) game.
+    g1 = gb.game_graph(aut, env='env', sys='sys', qinit=aut.qinit)
+    attributes = ['color', 'shape']
+    h1 = gb._game_format_nx(g1, attributes)
+    pd1 = nx.drawing.nx_pydot.to_pydot(h1)
+    pd1.write_pdf('runner_blocker_game.pdf')
+
+    # Making a graph pf the state transitions of the environment and system
+    g2 = gb.state_graph(aut, env='env', sys='sys', qinit=aut.qinit)
+    h2, _ = gb._state_format_nx(g2, attributes)
+    pd2 = nx.drawing.nx_pydot.to_pydot(h2)
+    pd2.write_pdf('runner_blocker_states.pdf')
+
+if __name__ == "__main__":
+    experiment()
+
