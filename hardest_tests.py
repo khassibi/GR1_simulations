32bd6cb16ff363e4d5bc23ac34daf03bcd386488
kimiahassibi <khassibi@caltech.edu>
2023-08-04 14:26:58 -0700

    added BFS tests

--- hardest_tests.py
+++ hardest_tests.py
@@ -38,3 +38,40 @@ def greedy_percent_red(g):
                 percent_red = (sys_node, counter/len(sys_actions))
         transitions[env_node] = percent_red[0]
     return transitions
+
+def BFS_most_red(g):
+    env_nodes = [node for node in g.nodes if 'oval' in g.nodes[node]['shape']]
+    transitions = dict()
+    for env_node in env_nodes:
+        env_actions = list(g.neighbors(env_node))
+        num_red = (env_actions[0], 0)
+        for sys_node in env_actions:
+            bfs_edges = list(nx.bfs_edges(g, source=sys_node))
+            bfs_nodes = [sys_node] + [v for _, v in bfs_edges]
+            counter = 0
+            for node in bfs_nodes:
+                if 'color' in g.nodes[node]:
+                    counter += 1
+            if counter > num_red[1]:
+                num_red = (sys_node, counter)
+        transitions[env_node] = num_red[0]
+    return transitions
+
+def BFS_percent_red(g):
+    env_nodes = [node for node in g.nodes if 'oval' in g.nodes[node]['shape']]
+    transitions = dict()
+    for env_node in env_nodes:
+        env_actions = list(g.neighbors(env_node))
+        percent_red = (env_actions[0], 0)
+        for sys_node in env_actions:
+            bfs_edges = list(nx.bfs_edges(g, source=sys_node))
+            bfs_nodes = [sys_node] + [v for _, v in bfs_edges]
+            counter = 0
+            num_nodes = len(bfs_nodes)
+            for node in bfs_nodes:
+                if 'color' in g.nodes[node]:
+                    counter += 1
+            if counter/num_nodes > percent_red[1]:
+                percent_red = (sys_node, counter/num_nodes)
+        transitions[env_node] = percent_red[0]
+    return transitions
