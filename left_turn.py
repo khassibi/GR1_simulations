f2c59552a7e0b72f563b30f4326030973c610c79
kimiahassibi <khassibi@caltech.edu>
2023-08-07 05:38:52 -0700

    fixed left_turn specifications

--- left_turn.py
+++ left_turn.py
@@ -5,8 +5,13 @@ from visualization import graph_builder as gb
 import networkx as nx
 from tulip.transys import machines
 
+from tulip import dumpsmach
+import pickle
+
 
 def experiment():
+    path = 'left_turn/'
+
     # System definition
     sys = tlp.transys.FTS()
 
@@ -21,7 +26,7 @@ def experiment():
     sys.transitions.add_comb({'c8'}, {'c8', 'c4'})
     ## Add remaining state transitions
     sys.transitions.add_comb({'c4'}, {'c4', 'c9'})
-    sys.transitions.add_comb({'c9'}, {'c9'}) # Should this transition exist
+    sys.transitions.add_comb({'c9'}, {'c9'})
 
     # Specifications for the environment
 
@@ -34,31 +39,37 @@ def experiment():
         'vh = 3 -> next(vh) = 3 | next(vh) = 4',
         'vh = 4 -> next(vh) = 4 | next(vh) = 5',
         'vh = 5 -> next(vh) = 5 | next(vh) = 6',
-        'vh = 6 -> next(vh) = 6' # Should I say always 6?
+        'vh = 6 -> next(vh) = 6',
     }
-    # QUESTION: should I use || instead of |?
     env_prog = {'vh = 6'}
 
     # Traffic light 
     env_vars.update({'light': ["g", "y", "r"]})
     env_init.update({'light = "g"'})
     env_safe |= {
-        'light = "g" -> next(light = "y")',
+        # 'light = "g" -> next(light = "y")',
         ## Add remaining light dynamics
         # 'light = "y" -> next(light = "y") | next(light = "r")',
         # 'light = "r" -> next(light = "r")'
         # 'light = "r" -> next(light = "r") | next(light = "g")| '
+        # 'light = "y" -> next(light = "r")',
+        # 'light = "r" -> next(light = "g")'
+        'light = "g" -> next(light = "g") | next(light = "y")',
         'light = "y" -> next(light = "r")',
-        'light = "r" -> next(light = "g")'
+        'light = "r" -> next(light = "r") | next(light = "g")'
     }
-    # env_prog = {'light = "r"'} # I added this because I think it should be there
-    # env_prog = {} 
+    env_prog |= {'light = "g"'} # I added this because I think it should be there
+    env_safe |= {
+        '!(light = "r" & (vh = 4 | vh = 5))'
+    }
 
     # System variables and requirements
     sys_vars = {}
     sys_init = {}
     sys_prog = {'a9'}
-    sys_safe = {'!(a4 & vh = 4)'}
+    sys_safe = {
+        '!(a4 & vh = 4)'
+    }
 
     specs = tlp.spec.GRSpec(env_vars, sys_vars, env_init, sys_init,
                             env_safe, sys_safe, env_prog, sys_prog)
@@ -70,30 +81,35 @@ def experiment():
     # Automaton class found in omega/omega/symbolic/temporal.py
     aut = omega_int._grspec_to_automaton(spec)
 
+    # Synthesize the controller
+    ctrl = tlp.synth.synthesize(specs, sys=sys)
+    assert ctrl is not None, 'unrealizable'
+
+    dumpsmach.write_python_case(path + 'controller.py', ctrl, classname="sys_ctrl")
+
     # Graphing
+    filename = path + "graph"
     attributes = ['color', 'shape']
     
     # Making a graph of the asynchronous GR(1) game with deadends.
     g0 = gb.game_graph(aut, env='env', sys='sys', remove_deadends=False, qinit=aut.qinit)
     h0 = gb._game_format_nx(g0, attributes)
     pd0 = nx.drawing.nx_pydot.to_pydot(h0)
-    pd0.write_pdf('left_turn/game.pdf')
+    pd0.write_pdf(path + 'game.pdf')
+    with open(filename, "wb") as file:
+        pickle.dump(g0, file)
 
     # Making a graph of the asynchronous GR(1) game without deadends.
     g1 = gb.game_graph(aut, env='env', sys='sys', remove_deadends=True, qinit=aut.qinit)
     h1 = gb._game_format_nx(g1, attributes)
     pd1 = nx.drawing.nx_pydot.to_pydot(h1)
-    pd1.write_pdf('left_turn/game_no_deadends.pdf')
+    pd1.write_pdf(path + 'game_no_deadends.pdf')
 
     # Making a graph pf the state transitions of the environment and system
     g2 = gb.state_graph(aut, env='env', sys='sys', qinit=aut.qinit)
     h2, _ = gb._state_format_nx(g2, attributes)
     pd2 = nx.drawing.nx_pydot.to_pydot(h2)
-    pd2.write_pdf('left_turn/states.pdf')
-
-    # Synthesize the controller
-    ctrl = tlp.synth.synthesize(specs, sys=sys)
-    assert ctrl is not None, 'unrealizable'
+    pd2.write_pdf(path + 'states.pdf')
 
 if __name__ == "__main__":
     experiment()
