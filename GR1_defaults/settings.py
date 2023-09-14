b9dc39033a25478a32eac071c6d9d108288d91a4
kimiahassibi <khassibi@caltech.edu>
2023-09-13 17:46:09 -0700

    an amalgamation

--- GR1_defaults/settings.py
+++ GR1_defaults/settings.py
@@ -8,7 +8,7 @@ def set_specs(env_vars, sys_vars, env_init, sys_init, env_safe, sys_safe,
     # Function found in tulip-control/tulip/spec/form.py
     specs = tlp.spec.GRSpec(env_vars, sys_vars, env_init, sys_init,
                             env_safe, sys_safe, env_prog, sys_prog)
-    specs.qinit = '\E \A'
-    specs.moore = False
+    specs.qinit = '\A \E'
+    specs.moore = False # mealy controller
     specs.plus_one = False
     return specs
