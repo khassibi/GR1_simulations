439dccf84a66d8ebdc28df0159409637df74b1c7
kimiahassibi <khassibi@caltech.edu>
2023-09-18 16:09:24 -0700

    removing /

--- left_turn_pedestrian.py
+++ left_turn_pedestrian.py
@@ -89,7 +89,7 @@ def experiment():
     # Synthesizing system controller
     ctrl = tlp.synth.synthesize(specs, sys=sys)
     assert ctrl is not None, 'unrealizable'
-    with open(path + "/ctrl", "wb") as file:
+    with open(path + "ctrl", "wb") as file:
         pickle.dump(ctrl, file)
 
     # time, states = ctrl.run('Sinit')
