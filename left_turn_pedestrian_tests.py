573e93231a2cc11c20f758049682957a3c91ff2e
kimiahassibi <khassibi@caltech.edu>
2023-09-21 09:27:10 -0700

    wow equal robustnesses

--- left_turn_pedestrian_tests.py
+++ left_turn_pedestrian_tests.py
@@ -17,38 +17,39 @@ def rand_tests(G, ctrl):
     # nodes
     title = "Memoryless Most Red"
     num_red_sys_metric = hard_tests.find_num_red_successors(G)
-    vh_signal, p_signal, light_signal = rand_test_with_metric(G, 0, hard_tests.memoryless_max_metric, num_red_sys_metric, 30)
+    vh_signal, p_signal, light_signal, trajectory = rand_test_with_metric(G, 0, hard_tests.memoryless_max_metric, num_red_sys_metric, 30)
     animate_test(ctrl, vh_signal, p_signal, light_signal, title)
 
     # Running the test that greedily picks the next state with the most unsafe 
     # nodes
     title = "Memoryless Percent Red"
     percent_red_sys_metric = hard_tests.find_percent_red_successors(G)
-    vh_signal, p_signal, light_signal = rand_test_with_metric(G, 0, hard_tests.memoryless_max_metric, percent_red_sys_metric, 30)
+    vh_signal, p_signal, light_signal, trajectory = rand_test_with_metric(G, 0, hard_tests.memoryless_max_metric, percent_red_sys_metric, 30)
     animate_test(ctrl, vh_signal, p_signal, light_signal, title)
 
     title = "Memoryless Min Robustness - Minimizing"
     min_robustness_env_metric = hard_tests.find_min_robustness(G)
-    vh_signal, p_signal, light_signal = rand_test_with_metric(G, 0, hard_tests.memoryless_robustness_minimization, min_robustness_env_metric, 30)
+    vh_signal, p_signal, light_signal, trajectory = rand_test_with_metric(G, 0, hard_tests.memoryless_robustness_minimization, min_robustness_env_metric, 30)
     animate_test(ctrl, vh_signal, p_signal, light_signal, title)
 
     title = "Memoryless Average Robustness - Minimizing"
     avg_robustness_env_metric = hard_tests.find_avg_robustness(G)
-    vh_signal, p_signal, light_signal = rand_test_with_metric(G, 0, hard_tests.memoryless_robustness_minimization, avg_robustness_env_metric, 30)
+    vh_signal, p_signal, light_signal, trajectory = rand_test_with_metric(G, 0, hard_tests.memoryless_robustness_minimization, avg_robustness_env_metric, 30)
     animate_test(ctrl, vh_signal, p_signal, light_signal, title)
 
     title = "Memoryless Min Robustness - Averaging"
     min_robustness_env_metric = hard_tests.find_min_robustness(G)
-    vh_signal, p_signal, light_signal = rand_test_with_metric(G, 0, hard_tests.memoryless_robustness_averaging, min_robustness_env_metric, 30)
+    vh_signal, p_signal, light_signal, trajectory = rand_test_with_metric(G, 0, hard_tests.memoryless_robustness_averaging, min_robustness_env_metric, 30)
     animate_test(ctrl, vh_signal, p_signal, light_signal, title)
 
     title = "Memoryless Average Robustness - Averaging"
     avg_robustness_env_metric = hard_tests.find_avg_robustness(G)
-    vh_signal, p_signal, light_signal = rand_test_with_metric(G, 0, hard_tests.memoryless_robustness_averaging, avg_robustness_env_metric, 30)
+    vh_signal, p_signal, light_signal, trajectory = rand_test_with_metric(G, 0, hard_tests.memoryless_robustness_averaging, avg_robustness_env_metric, 30)
     animate_test(ctrl, vh_signal, p_signal, light_signal, title)
 
 def rand_test_with_metric(G, init_node, test, metric, max_runs):
     curr_num = init_node
+    trajectory = [curr_num]
 
     # Initializing the initial state
     init_state = G.nodes[curr_num]
@@ -89,59 +90,12 @@ def rand_test_with_metric(G, init_node, test, metric, max_runs):
         env_state.update({'vh': vh, "p": p, 'light': light, 'shape': 'oval'})
 
         curr_num = list(G.nodes.values()).index(env_state)
+        trajectory.append(curr_num)
 
         curr_num = random.choice(test_transitions[curr_num])
         counter += 1
     
-    return vh_signal, p_signal, light_signal
-
-def run_every_test(G, init_node, test, metric, max_runs):
-    curr_num = init_node
-
-    # Initializing the initial state
-    init_state = G.nodes[curr_num]
-    vh = init_state['vh']
-    p = init_state['p']
-    light = init_state['light']
-    
-    # Initializing signals
-    vh_signal = [vh]
-    p_signal = [p]
-    light_signal = [light]
-
-    # Finding the transitions for the test
-    test_transitions = test(G, metric)
-    
-    sys_control = sys_ctrl()
-    env_state = sys_ctrl.move(sys_control, vh, p, light)
-    env_state.update({'vh': vh, "p": p, 'light': light, 'shape': 'oval'})
-    assert env_state == init_state, (env_state, init_state)
-
-    curr_num = random.choice(test_transitions[curr_num])
-    # curr_num = test_transitions[curr_num][0]
-
-    # Running the test
-    counter = 0
-    while env_state['a9'] == False and counter < max_runs:
-        sys_state = G.nodes[curr_num]
-        vh = sys_state['vh']
-        p = sys_state['p']
-        light = sys_state['light']
-
-        # Keeping track of signals
-        vh_signal.append(vh)
-        p_signal.append(p)
-        light_signal.append(light)
-
-        env_state = sys_ctrl.move(sys_control, vh, p, light)
-        env_state.update({'vh': vh, "p": p, 'light': light, 'shape': 'oval'})
-
-        curr_num = list(G.nodes.values()).index(env_state)
-
-        curr_num = random.choice(test_transitions[curr_num])
-        counter += 1
-    
-    return vh_signal, p_signal, light_signal
+    return vh_signal, p_signal, light_signal, trajectory
 
 def animate_test(ctrl, vh_signal, p_signal, light_signal, title):
     time, states = ctrl.run('Sinit', {'light': light_signal, 'vh': vh_signal, 
